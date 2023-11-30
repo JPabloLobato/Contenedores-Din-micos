406cf4fa62007f7bf01522814c4e8c6afe942231
JPabloLobato <jlobagon@myuax.com>
2023-11-30 10:52:43 +0100

    Vuelta EJ2

--- EJ2.cpp
+++ EJ2.cpp
@@ -1,23 +1,3 @@
 #include <iostream>
 #include <list>
-#include <strack>
-
-int main () {
-    std::list<float> lista = {1.1, 2.2, 3.3, 4.4, 5.5};
-    std::stack<float, std::list<float>> pila(lista);
-    std::cout << "Pila inicial: " << std::endl;
-    while (!pila.empty()) {
-        std::cout << pila.top() << std::endl;
-        pila.pop();
-    }
-    pila.push(6.6);
-    pila.push(7.7);
-    pila.push(8.8);
-    std::cout "\nPila despues de agregar 3 elementos: " << std::endl;
-    while (!pila.empty()) {
-
-        std::cout << pila.top() << std::endl;
-        pila.pop();
-    }
-    return 0;
-}
+#include <stack>
---
+++ EJ3.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Pablo Lobato on 30/11/2023.
+//
+
+#include "EJ3.h"
