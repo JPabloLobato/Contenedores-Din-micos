ce1f2ce9e8f6d459edccd638999a9de084025843
JPabloLobato <jlobagon@myuax.com>
2023-11-30 10:25:52 +0100

    Error EJ2

--- EJ2.cpp
+++ EJ2.cpp
@@ -1,5 +1,23 @@
-//
-// Created by Pablo Lobato on 30/11/2023.
-//
+#include <iostream>
+#include <list>
+#include <strack>
 
-#include "EJ2.h"
+int main () {
+    std::list<float> lista = {1.1, 2.2, 3.3, 4.4, 5.5};
+    std::stack<float, std::list<float>> pila(lista);
+    std::cout << "Pila inicial: " << std::endl;
+    while (!pila.empty()) {
+        std::cout << pila.top() << std::endl;
+        pila.pop();
+    }
+    pila.push(6.6);
+    pila.push(7.7);
+    pila.push(8.8);
+    std::cout "\nPila despues de agregar 3 elementos: " << std::endl;
+    while (!pila.empty()) {
+
+        std::cout << pila.top() << std::endl;
+        pila.pop();
+    }
+    return 0;
+}
