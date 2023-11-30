2edc9e573b8cc0b5a747c0d26e819344039a9487
JPabloLobato <jlobagon@myuax.com>
2023-11-30 11:21:36 +0100

    Error CMakeList

--- EJ3.cpp
+++ EJ3.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
+int main() {
+    std::vector<int> numeros = {50, 20, 60, 10, 30, 40};
+    std::sort(numeros.begin(), numeros.end());
+    std::cout << "Elementos ordenados:" << std::endl;
+    for (int num : numeros) {
+        std::cout << num << std::endl;
+    }
+    if (!numeros.empty()) {
+        int maxNum = *std::max_element(numeros.begin(), numeros.end());
+        std::cout << "Número máximo: " << maxNum << std::endl;
+    } else {
+        std::cout << "El vector está vacío." << std::endl;
+    }
+
+    return 0;
+}
