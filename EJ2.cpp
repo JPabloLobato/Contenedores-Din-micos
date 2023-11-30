f815fe8c11189cf031625afcd1e772b78c36c165
JPabloLobato <jlobagon@myuax.com>
2023-11-30 10:54:11 +0100

    EJ2

--- EJ2.cpp
+++ EJ2.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 #include <list>
 #include <stack>
+
+int main() {
+    std::list<float> lista = {1.1, 2.2, 3.3, 4.4, 5.5};
+    std::stack<float, std::list<float>> pila(lista);
+    std::cout << "Pila inicial:" << std::endl;
+    while (!pila.empty()) {
+        std::cout << pila.top() << std::endl;
+        pila.pop();
+    }
+    pila.push(6.6);
+    pila.push(7.7);
+    pila.push(8.8);
+    std::cout << "\nPila después de agregar elementos:" << std::endl;
